bb38992eaaa9cf3710a2496946314567ccd96a6a
mparje <126131806+mparje@users.noreply.github.com>
2023-06-06 08:32:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ else:
     openai.api_key = api_key
     # Continuar con el resto del código que utiliza la clave de API
 
-# Genera el resumen utilizando GPT-3
+# Generar el resumen utilizando GPT-3
 def generate_summary(pdf_path):
-    # Leer el contenido del PDF (puedes utilizar una biblioteca como PyPDF2 o pdfminer.six)
-    # y almacenarlo en una variable llamada 'content'
+    # Leer el contenido del PDF y almacenarlo en una variable llamada 'content'
+    content = read_pdf_content(pdf_path)
 
     # Llamar a la API de GPT-3 para generar el resumen
     response = openai.Completion.create(
@@ -33,21 +33,28 @@ def generate_summary(pdf_path):
     return summary
 
 
-# Genera el archivo PPTX utilizando el resumen y las imágenes
+# Leer el contenido del PDF
+def read_pdf_content(pdf_path):
+    # Implementa la lógica para leer el contenido del PDF y devolverlo como una cadena de texto
+    # Puedes utilizar una biblioteca como PyPDF2 o pdfminer.six
+    pass
+
+
+# Generar el archivo PPTX utilizando el resumen y las imágenes
 def generate_pptx(summary, images, pptx_path):
     presentation = Presentation()
 
-    # Agrega el resumen como el contenido de la primera diapositiva
+    # Agregar el resumen como el contenido de la primera diapositiva
     slide = presentation.slides.add_slide(presentation.slide_layouts[1])
     slide.shapes.title.text = 'Resumen del PDF'
     slide.placeholders[1].text = summary
 
-    # Agrega las imágenes a las diapositivas
+    # Agregar las imágenes a las diapositivas
     for image_path in images:
         slide = presentation.slides.add_slide(presentation.slide_layouts[1])
         slide.shapes.add_picture(image_path, Inches(1), Inches(1), width=Inches(8), height=Inches(6))
 
-    # Guarda la presentación como un archivo PPTX
+    # Guardar la presentación como un archivo PPTX
     presentation.save(pptx_path)
 
 
@@ -57,24 +64,24 @@ def main():
     pdf_file = st.file_uploader("Cargar archivo PDF", type=["pdf"])
 
     if pdf_file is not None:
-        # Guarda el archivo PDF en el sistema de archivos
+        # Guardar el archivo PDF en el sistema de archivos
         with open('temp.pdf', 'wb') as f:
             f.write(pdf_file.getvalue())
 
-        # Genera el resumen del PDF
+        # Generar el resumen del PDF
         summary = generate_summary('temp.pdf')
 
-        # Convierte las páginas del PDF a imágenes (puedes utilizar la biblioteca pdf2image)
+        # Convertir las páginas del PDF a imágenes
         images = convert_pdf_to_images('temp.pdf')
 
-        # Genera la presentación PPTX utilizando el resumen y las imágenes
+        # Generar la presentación PPTX utilizando el resumen y las imágenes
         generate_pptx(summary, images, 'output.pptx')
 
-        # Descarga el archivo PPTX
+        # Descargar el archivo PPTX
         with open('output.pptx', 'rb') as f:
             st.download_button('Descargar PPTX', f.read(), file_name='output.pptx')
 
-        # Elimina los archivos temporales
+        # Eliminar los archivos temporales
         os.remove('temp.pdf')
         os.remove('output.pptx')
 
